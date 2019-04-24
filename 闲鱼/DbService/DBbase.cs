26a4926e2bdc41555b4df99a3830ad6f9ff25de2
dahe <649761355@qq.com>
2019-04-24 17:55:35 +0800

    add ConfigureHelper.cs and dapper

---
+++ 闲鱼/DbService/DBbase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace 闲鱼.DbService
+{
+    public class DBbase
+    {
+        static private SqlConnection _connection;
+        static SqlConnection connection { get {
+                if (_connection == null)
+                {
+                    _connection =  new SqlConnection(Common.ConfigureHelper.Read("ConnectionStr"));
+                }
+                return _connection;
+            } }
+        public DBbase()
+        {
+           
+        }
+        public static void Execute(string Sql)
+        {
+            try
+            {
+                connection.Execute(Sql);
+            }
+            catch (Exception er)
+            {
+
+                throw er;
+            }
+        }
+    }
+}
